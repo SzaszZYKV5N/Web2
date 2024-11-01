3627e2fe59d06df681dad6be56bcce3f7cf95e4e
SzaszAndrasGabor <szaszandras71@gmail.com>
2024-11-01 18:49:21 +0100

    Tartalom feltöltés
    Tartalom feltöltés, képek hozzáadása, munkatársak oldal létrehozása

--- views/elerhetoseg_alapinfok_main.php
+++ views/elerhetoseg_alapinfok_main.php
@@ -1,5 +1,23 @@
-<h2>
-    <br>Alapinfók:<br>
-    <br>Alapinfó 1<br>
-    <br>Alapinfó 2<br>
-</h2>
+
+
+<div class="about_section layout_padding margim_90">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                    <h2>TISZTA VÍZ Környezetvédelmi és Szolgáltató Korlátolt Felelősségű Társaság  </h2>
+
+                    <p class="about_text">Székhelye: 1184 Budapest, Kubinyi Imre utca 9.</p>
+                    <p class="about_text">Alakult: 2003. március 21. (tehát 21. éve működő mikrovállalkozás)</p>
+                    <p class="about_text">Fő tevékenysége: 3700’08 Szennyvíz gyűjtése, kezelése: csatornatisztítás, csatornakamerázás, csatornadiagnosztika, szakvéleményezés, duguláselhárítás, szivattyúzás</p>
+                    <p class="about_text">Cégjegyzékszáma: 01-09-715040</p>
+                    <p class="about_text">Adószáma: 13022718-2-43</p>
+                    <p class="about_text">Közösségi adószáma: HU13022718</p>
+                    <p class="about_text">OTP Bank: 11703006-20422488</p>
+                  
+                    </div>
+               <div class="col-md-6">
+                  <div class="about_img"><img src="images/jog.jpg"></div>
+               </div>
+            </div>
+         </div>
+      </div>
--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -3,13 +3,13 @@
          <div class="container">
             <div class="row">
                <div class="col-md-6">
-                  <h1 class="about_taital">Vízvezeték-szerelők</h1>
+                  <h1 class="about_taital">Üdvözöljük honlapunkon!</h1>
                   <p class="about_text">A sárgahegyi Tiszta Víz Kft. hosszú ideje áll a lakosság szolgálatában.</p>
                   <p class="about_text">Munkatársai az építkezéseken végzett munka mellett bármilyen problémát gyorsan orvosolnak, legyen szó csöpögő csapról vagy csőtörésről.</p>
                   
                </div>
                <div class="col-md-6">
-                  <div class="about_img"><img src="images/about-img.png"></div>
+                  <div class="about_img"><img src="images/szereles.jpg"></div>
                </div>
             </div>
          </div>
