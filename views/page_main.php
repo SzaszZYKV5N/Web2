c287623864993716287faa5dc92065ba21bcea36
SzaszAndrasGabor <szaszandras71@gmail.com>
2024-11-01 21:10:45 +0100

    Update page_main.php

--- views/page_main.php
+++ views/page_main.php
@@ -47,7 +47,7 @@
                   <div class="header_top_main">
                      <div class="call_text"><a href="#"><span class="padding_right0"><i class="fa fa-phone" aria-hidden="true"></i></span>  Call : +01 1234567890</a></div>
                      <div class="call_text_2"><a href="#"><span class="padding_right0"><i class="fa fa-envelope" aria-hidden="true"></i></span> demo@gmail.com</a></div>
-                     <div class="call_text_1"><a href="#"><span class="padding_right0"><i class="fa fa-map-marker" aria-hidden="true"></i></span> Mindenhol ott vagyunk!</a></div>
+                     <div class="call_text_1"><a href="#"><span class="padding_right0"><i class="fa fa-map-user" aria-hidden="true"></i></span> <?= $_SESSION['userlastname']." ".$_SESSION['userfirstname'] ?></div>
                   </div>
                </div>
             </div>
