0a7c556cdcdb5df3afc500013c0aca0b9655bad8
SzaszZYKV5N <szasz.andras002@gmail.com>
2024-11-24 09:07:22 +0100

    Kód tistázás

--- controllers/beleptet.php
+++ controllers/beleptet.php
@@ -2,17 +2,17 @@
 
 class Beleptet_Controller
 {
-	public $baseName = 'beleptet';  //meghat�rozni, hogy melyik oldalon vagyunk
-	public function main(array $vars) // a router �ltal tov�bb�tott param�tereket kapja
+	public $baseName = 'beleptet';  //meghatározni, hogy melyik oldalon vagyunk
+	public function main(array $vars) // a router által továbbított paramétereket kapja
 	{
-		$beleptetModel = new Beleptet_Model;  //az oszt�lyhoz tartoz� modell
-		//a modellben bel�pteti a felhaszn�l�t
+		$beleptetModel = new Beleptet_Model;  //az osztályhoz tartozó modell
+		//a modellben belépteti a felhasználót
 		$retData = $beleptetModel->get_data($vars);
 		if($retData['eredmeny'] == "ERROR")
 			$this->baseName = "belepes";
-		//bet�ltj�k a n�zetet
+		//betöltjük a nézetet
 		$view = new View_Loader($this->baseName.'_main');
-		//�tadjuk a lek�rdezett adatokat a n�zetnek
+		//Átadjuk a lekérdezett adatokat a nézetnek
 		foreach($retData as $name => $value)
 			$view->assign($name, $value);
 	}
